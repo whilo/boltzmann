b05b8c3e1aec332b7b8f63d53b8162359cee4aeb
Christian Weilbach <christian_weilbach@web.de>
2014-09-24 02:41:16 +0200

    Load all label data.

--- src/boltzmann/mnist.clj
+++ src/boltzmann/mnist.clj
@@ -6,13 +6,15 @@
 
 
 (defn read-labels [file]
-  (let [header (compile-frame {:magic :int32,
-                               :number-of-items :int32})]
-    (decode header (byte-array (take 8 (to-byte-array (io/input-stream file)))))))
+  (let [labels (compile-frame
+                (ordered-map :magic :int32,
+                             :labels (finite-frame :int32
+                                                   (repeated :byte :prefix :none))))]
+    (decode labels (to-byte-array (io/input-stream file)))))
 
 
 (comment
-  (read-labels "resources/train-labels-idx1-ubyte")
+  (:magic (read-labels "resources/train-labels-idx1-ubyte"))
   ;; example from gloss wiki
   (def fr (compile-frame {:a :int16, :b :float32}))
 
