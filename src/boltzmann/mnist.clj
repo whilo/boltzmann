fab8f6b5675be7c077e501415419379a80ea8286
Christian Weilbach <christian_weilbach@web.de>
2014-09-28 02:10:10 +0200

    Use flat arrays for visible units; tiled rendering.

--- src/boltzmann/mnist.clj
+++ src/boltzmann/mnist.clj
@@ -3,10 +3,14 @@
              [gloss.core :refer [defcodec compile-frame finite-frame
                                  ordered-map repeated]]
              [gloss.io :refer [encode decode]]
-             [clojure.java.io :refer [input-stream]]))
+             [clojure.java.io :refer [input-stream file]])
+  (:import [javax.imageio ImageIO]))
 
+;; for gzip have a look at https://gist.github.com/bpsm/1858654
 
-(defn read-labels [file]
+(defn read-labels
+  "Reads a label file for MNIST digits and returns a list of integers."
+  [file]
   (let [labels (compile-frame
                 (ordered-map :magic :int32,
                              :labels (finite-frame :int32
@@ -17,9 +21,12 @@
          (decode labels)
          :labels)))
 
-(defcodec raw-image (repeat 28 (repeat 28 :ubyte)))
+(defcodec raw-image (repeat (* 28 28) :ubyte))
 
-(defn read-images [file]
+(defn read-images
+  "Reads a file of MNIST digits and returns a list of float-arrays for each image,
+  containing gray-scale pixel values between 0 and 1."
+  [file]
   (let [buffer (to-byte-buffer (input-stream file))
         images-frame (compile-frame
                       (ordered-map :magic :int32,
@@ -28,18 +35,16 @@
                                    :columns :int32,
                                    :images (repeated raw-image :prefix :none)))
         raw-images (decode images-frame buffer)]
-    (doall (mapv
-            (fn [img] (mapv
-                      (fn [row]
-                        ;; float-array instead of pvec keeps us below
-                        ;; 2 gig heap atm., take care with "dot"
-                        ;; dot-product TODO
-                        (float-array (mapv (fn [pixel]
-                                             (float (/ (float pixel) 256)))
-                                           row)))
-                      img))
-            (:images raw-images)))))
+    (doall (mapv (fn [img] (float-array (map #(float (/ (float %) 256)) img)))
+                 (:images raw-images)))))
 
+
+(defn horizontal-tile [float-matrices] ;; TODO generalize to vertical
+  (->> float-matrices
+       (apply interleave)
+       (apply concat)
+       (partition (* (count float-matrices)
+                     (count (first float-matrices))))))
 
 
 (defn view [image]
@@ -53,11 +58,12 @@
     (.setVisible true)))
 
 
-(defn draw-image [image]
-  (let [w (count (first image))
-        h (count image)
-        buffered-image (java.awt.image.BufferedImage. w h java.awt.image.BufferedImage/TYPE_BYTE_GRAY)
-        coords (for [x (range w) y (range h)] [x y (get-in image [y x])])]
+(defn render-grayscale-float-matrix [m]
+  (let [w (count (first m))
+        h (count m)
+        buffered-image (java.awt.image.BufferedImage.
+                        w h java.awt.image.BufferedImage/TYPE_BYTE_GRAY)
+        coords (for [x (range w) y (range h)] [x y (-> m (nth y) (nth x))])]
     (doseq [[x y c] coords]
       (.setRGB buffered-image x y
                (.getRGB (java.awt.Color. (float c) (float c) (float c)))))
@@ -68,8 +74,13 @@
 (comment
   (def images (read-images "resources/train-images-idx3-ubyte"))
 
-  (-> (images 1)
-      draw-image
-      view)
+  (def rendered (->> (take 30 images)
+                     (map #(partition 28 %))
+                     horizontal-tile
+                     render-grayscale-float-matrix))
+
+  (view rendered)
+
+  (ImageIO/write rendered "png" (file "/tmp/mnist.png"))
 
   (def labels (read-labels "resources/train-labels-idx1-ubyte")))
